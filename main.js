3dd625b6e32b56b532458aa21f01f85f640daabc
officialas807 <140934926+officialas807@users.noreply.github.com>
2023-07-30 10:43:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function ytvid() {
+    window.location="youtube.html";
+}
+
+function ytreel() {
+    window.location="ytreel.html";
+}
+
+function facevid() {
+    window.location="facebook.html";
+}
+
+function facereel() {
+    window.location="facebook-reels.html";
+}
+
+function instavid() {
+    window.location="insta.html";
+}
+
+function instareel() {
+    window.location="instareel.html";
+}
